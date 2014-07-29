51bb7342a94ea89fe134d7c95520b3dc761cf7e7
Justin Ramos <justin.ramos@gmail.com>
2014-07-29 14:32:36 -0500

    initial spike

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem 'rails', '4.1.4'
 gem 'haml-rails',   '~> 0.5.3'
 gem 'sass-rails',   '~> 4.0.3'
 gem 'jquery-rails', '~> 3.1.1'
+
+gem 'cgminer_api_client', path: '../cgminer_api_client'
--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,4 +1,22 @@
 class ManagerController < ApplicationController
+  before_filter :load_miner_pool
+  before_filter :setup_summary
+
   def index
+  end
+
+  private
+  
+  def load_miner_pool
+    @miner_pool = CgminerApiClient::MinerPool.new
+  end
+  
+  def setup_summary
+    @summary = {
+      :ghs_avg     => [],
+      :ghs_5s      => [],
+      :error_rate  => [],
+      :temperature => []
+    }
   end
 end
