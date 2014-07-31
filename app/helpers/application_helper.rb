3e5e3b25de7cc3416d026fe22d20bbf6c52bec9f
Justin Ramos <justin.ramos@gmail.com>
2014-07-31 09:01:00 -0500

    adding formatHashrate helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def formatHashrate(rate)
+    rate = rate.to_f
+    unit = 'H/s'
+
+    if (rate >= 1000)
+      rate /= 1000; unit = 'KH/s'
+    end
+
+    if(rate >= 1000)
+      rate /= 1000; unit = 'MH/s'
+    end
+
+    if(rate >= 1000)
+      rate /= 1000; unit = 'GH/s'
+    end
+
+    if(rate >= 1000)
+      rate /= 1000; unit = 'TH/s'
+    end
+
+    if(rate >= 1000)
+      rate /= 1000; unit = 'PH/s'
+    end
+
+    return (rate.round(2).to_s + ' ' + unit);
+  end
 end
