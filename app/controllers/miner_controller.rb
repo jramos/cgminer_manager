2dd33540cd8f82277ce11f9de7d925614c5f6499
Justin Ramos <justin.ramos@gmail.com>
2014-08-04 14:58:04 -0500

    wait for pool to be disabled; preload_app

--- app/controllers/miner_controller.rb
+++ app/controllers/miner_controller.rb
@@ -33,8 +33,14 @@ class MinerController < ApplicationController
       orig_pool_ids.each do |pool_id|
         begin
           @miner.disablepool(pool_id)
-          sleep(1)
-          @miner.removepool(pool_id)
+
+          while(true)
+            pool = @miner.pools.detect{|pool| pool[:pool].to_s == pool_id.to_s}
+            if pool[:status] == 'Disabled'
+              @miner.removepool(pool_id)
+              break
+            end
+          end
         rescue Exception => e
           logger.info "Couldn't disable/remove pool #{pool_id}: #{e.message}"
         end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,7 @@
 listen 3000
 timeout 120
 worker_processes 4
+preload_app true
 
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
