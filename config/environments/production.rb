39cdb6e8cabb5239a503da1544fb523953e58501
Justin Ramos <justin.ramos@gmail.com>
2014-08-08 14:58:59 -0500

    correcting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
-  # Fallback to assets pipeline if a precompiled asset is missed.
+  # Don't fallback on asset pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # Generate digests for assets URLs.
