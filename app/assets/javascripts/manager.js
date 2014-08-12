3290f55239ad95a47729dc0bbd31df2c22a1c647
Justin Ramos <justin.ramos@gmail.com>
2014-08-11 20:06:52 -0500

    warning siren

--- app/assets/javascripts/manager.js
+++ app/assets/javascripts/manager.js
@@ -1,3 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+function playWarningSound() {
+    warning = $("#audio-warning")[0];
+
+    if (warning && warning.play) {
+        warning.play();
+    }
+}
