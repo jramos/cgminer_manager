954c4a910cebdeff7ab2d11d0050571839af3559
Justin Ramos <justin.ramos@gmail.com>
2014-08-13 20:51:49 -0500

    misc improvements

--- app/assets/javascripts/manager.js
+++ app/assets/javascripts/manager.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var goToMiner = function(miner_index) {
+  document.location='/miner/' + miner_index.toString();
+}
