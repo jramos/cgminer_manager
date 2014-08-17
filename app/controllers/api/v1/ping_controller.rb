555294934d8402e795e48575348c92477165acb1
Justin Ramos <justin.ramos@gmail.com>
2014-08-17 16:24:04 -0500

    don't need to force reload

--- app/controllers/api/v1/ping_controller.rb
+++ app/controllers/api/v1/ping_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PingController < ApplicationController
   def index
     render :json => {
       :timestamp => Time.now.to_i,
-      :available_miners => @miner_pool.available_miners(true).count,
+      :available_miners => @miner_pool.available_miners.count,
       :unavailable_miners => @miner_pool.unavailable_miners.count
     }
   end
