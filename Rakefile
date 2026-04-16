81a8d0997ef70fd4e51e42057137021e07b17603
Justin Ramos <justin.ramos@gmail.com>
2026-04-16 16:40:35 -0600

    chore: rubocop + rspec scaffolding, empty green suite

--- Rakefile
+++ Rakefile
@@ -1,6 +1,9 @@
-# Add your own tasks in files placed in lib/tasks ending in .rake,
-# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+# frozen_string_literal: true
 
-require File.expand_path('../config/application', __FILE__)
+require 'rspec/core/rake_task'
+require 'rubocop/rake_task'
 
-Rails.application.load_tasks
+RSpec::Core::RakeTask.new(:spec)
+RuboCop::RakeTask.new
+
+task default: %i[rubocop spec]
---
+++ spec/spec_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/vendor/'
+  add_filter '/app/'       # legacy Rails tree excluded from coverage math
+  minimum_coverage line: 80
+end
+
+ENV['RACK_ENV'] ||= 'test'
+
+require 'cgminer_manager'
+
+Dir[File.join(__dir__, 'support', '**', '*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.mock_with :rspec do |c|
+    c.verify_partial_doubles = true
+  end
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.define_derived_metadata(file_path: %r{/spec/integration/}) do |meta|
+    meta[:integration] = true
+  end
+end
